a1fd29af94314bd58ea0ee8977e492aed1066463
MUHAMMED-SAHAL <123559222+MUHAMMED-SAHAL@users.noreply.github.com>
2024-01-10 21:06:15 +0530

    init

---
+++ arucoGenerator.py
@@ -0,0 +1,47 @@
+import cv2
+import matplotlib.pyplot as plt
+
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+
+aruco_type = ARUCO_DICT["DICT_6X6_50"]
+
+arucoDict = cv2.aruco.getPredefinedDictionary(aruco_type)
+
+id = 40 # This is the identifier of the bookmark
+img_size = 250 # Define the size of the final image
+marker_img = cv2.aruco.generateImageMarker(arucoDict, id, img_size)
+
+# Display the marker using matplotlib (optional)
+plt.imshow(marker_img, cmap='gray', interpolation="nearest")
+plt.axis("off")
+plt.show()
+
+# save the image
+cv2.imwrite("aruco{}.png".format(id), marker_img)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
